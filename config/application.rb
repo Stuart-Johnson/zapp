8fc3be235a525ada696999c8620828a7f3503752
Stuart Johnson <work.stuart.johnson@gmail.com>
2024-09-16 23:21:03 -0400

    cleanup

--- config/application.rb
+++ config/application.rb
@@ -23,10 +23,5 @@ module Zapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    
-    # config.generators do |g|
-    #   g.template_engine :haml
-    # end
-
   end
 end
